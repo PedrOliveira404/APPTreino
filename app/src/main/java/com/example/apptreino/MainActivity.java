ad14d554ebca4259e05baccd01de17ccefdbd2a4
Pedro Oliveira <pedroliveiracc@gmail.com>
2022-04-13 20:58:50 -0300

    APP Pronto

--- app/src/main/java/com/example/apptreino/MainActivity.java
+++ app/src/main/java/com/example/apptreino/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
                 mostrarMenu();
             }
-        }, 1500);
+        }, 2000);
     }
 
 
